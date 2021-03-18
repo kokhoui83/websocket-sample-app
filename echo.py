60b3d9b3eea2ee50997c81d46bb59b3db7e7bcb4
brian lim <kokhoui.lim@duerrdental.com>
2021-03-18 11:43:35 +0800

    add echo code. add readme and gitignore file

--- echo.py
+++ echo.py
@@ -3,34 +3,34 @@ import websockets
 import json
 
 async def send_string(message):
-    uri = "ws://echo.websocket.org"
+    uri = 'ws://echo.websocket.org'
     async with websockets.connect(uri) as websocket:
         await websocket.send(message)
-        print(f"> sending string")
+        print(f'> sending string')
 
         response = await websocket.recv()
-        print(f"echo received < {response}")
+        print(f'echo received < {response}')
 
 async def send_number(value):
-    uri = "ws://echo.websocket.org"
+    uri = 'ws://echo.websocket.org'
     async with websockets.connect(uri) as websocket:        
         await websocket.send(str(value))
-        print(f"> sending number")
+        print(f'> sending number')
 
         response = await websocket.recv()
-        print(f"echo received < {response}")
+        print(f'echo received < {response}')
 
 async def send_json(filename):
-    uri = "ws://echo.websocket.org"
+    uri = 'ws://echo.websocket.org'
     async with websockets.connect(uri) as websocket:
         with open(filename, 'r') as f:
             data = json.load(f)
         
         await websocket.send(json.dumps(data))
-        print(f"> sending JSON object")
+        print(f'> sending JSON object')
 
         response = await websocket.recv()
-        print(f"echo received < {response}")
+        print(f'echo received < {response}')
 
 asyncio.get_event_loop().run_until_complete(send_string('hello'))
 asyncio.get_event_loop().run_until_complete(send_number(1234))
